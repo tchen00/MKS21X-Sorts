26fd8d232999cb9261aeed08606381cf09a27ad1
Tammy Chen <tchen00@stuy.edu>
2018-12-04 20:43:48 -0500

    add selection sort

--- sorts.java
+++ sorts.java
@@ -1,7 +1,22 @@
 public class sorts{
 
   public static void main(String[] args){
-    
+
+  }
+  
+  public static void selectionsort(int [] ary) {
+    for (int i = 0; i < ary.length; i++) {
+      int original = ary[i];
+      int[] criteria = [original, i];
+      for (int j = 0; j < ary.length; j++) {
+        if (ary[j] < criteria[0]) {
+          criteria[0] = ary[j];
+          criteria[1] = j;
+        }
+      }
+      ary[i] = criteria[0];
+      ary[criteria[1]] = original;
+    }
   }
 
 
