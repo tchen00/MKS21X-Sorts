e7c74843c45dde2bcd922394907e6397c350c128
Tammy Chen <tchen00@stuy.edu>
2018-12-04 20:50:39 -0500

    fixed syntax error and tested code

--- sorts.java
+++ sorts.java
@@ -1,24 +1,24 @@
 public class sorts{
 
-  public static void main(String[] args){
-
-  }
-  
   public static void selectionsort(int [] ary) {
+    // looping through the first time
     for (int i = 0; i < ary.length; i++) {
+      // original keeps track of the value at the first index
       int original = ary[i];
-      int[] criteria = [original, i];
-      for (int j = 0; j < ary.length; j++) {
+      // criteria has two things, the minimum and the index of the min
+      int[] criteria = new int[] {original, i};
+      for (int j = i + 1; j < ary.length; j++) {
+        // checks to see the the value at index j is smaller than that of the criteria right now
         if (ary[j] < criteria[0]) {
           criteria[0] = ary[j];
           criteria[1] = j;
         }
       }
+      // replace + repeat once again 
       ary[i] = criteria[0];
       ary[criteria[1]] = original;
     }
   }
-
 
 
 }
